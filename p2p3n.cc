40e9121381d46e892c06f04d2ef45125f0e6d163
sgb-2699 <167523788+sgb-2699@users.noreply.github.com>
2025-01-14 20:16:42 +0530

    Update p2p3n.cc

--- p2p3n.cc
+++ p2p3n.cc
@@ -2,79 +2,80 @@
 #include "ns3/network-module.h"
 #include "ns3/internet-module.h"
 #include "ns3/point-to-point-module.h"
-#include "ns3/applications-module.h"
-#include "ns3/mobility-module.h"
 #include "ns3/netanim-module.h"
+#include "ns3/applications-module.h"
 
 using namespace ns3;
 
-NS_LOG_COMPONENT_DEFINE("ThreeNodeP2P");
+NS_LOG_COMPONENT_DEFINE("P2PThreeNodes");
 
 int main() {
-    Time::SetResolution(Time::NS);
-    LogComponentEnable("UdpEchoClientApplication", LOG_LEVEL_INFO);
-    LogComponentEnable("UdpEchoServerApplication", LOG_LEVEL_INFO);
+	LogComponentEnable("UdpEchoServerApplication", LOG_LEVEL_INFO);
+	LogComponentEnable("UdpEchoClientApplication", LOG_LEVEL_INFO);
 
-    NodeContainer nodes;
-    nodes.Create(3);
+	NodeContainer nodes;
+	nodes.Create(3);
 
-    NodeContainer n0n1 = NodeContainer(nodes.Get(0), nodes.Get(1));
-    NodeContainer n2n1 = NodeContainer(nodes.Get(2), nodes.Get(1));
+	PointToPointHelper pointToPoint;
+	pointToPoint.SetDeviceAttribute("DataRate", StringValue("5Mbps"));
+	pointToPoint.SetChannelAttribute("Delay", StringValue("2ms"));
 
-    pointToPoint.SetDeviceAttribute("DataRate", StringValue("5Mbps"));
-    pointToPoint.SetChannelAttribute("Delay", StringValue("2ms"));
+	NetDeviceContainer devices01;
+	devices01 = pointToPoint.Install(nodes.Get(0), nodes.Get(1));
 
-    NetDeviceContainer devices1 = pointToPoint.Install(n0n1);
-    NetDeviceContainer devices2 = pointToPoint.Install(n2n1);
+	NetDeviceContainer devices02;
+	devices02 = pointToPoint.Install(nodes.Get(0), nodes.Get(2));
 
-    AsciiTraceHelper ascii;
-    pointToPoint.EnableAsciiAll(ascii.CreateFileStream("Second.tr"));
+	InternetStackHelper stack;
+	stack.Install(nodes);
 
-    InternetStackHelper stack;
-    stack.Install(nodes);
+	Ipv4AddressHelper address01;
+	address01.SetBase("10.1.1.0", "255.255.255.0");
+	Ipv4InterfaceContainer interfaces01 = address01.Assign(devices01);
 
-    Ipv4AddressHelper address;
-    address.SetBase("10.1.1.0", "255.255.255.0");
-    Ipv4InterfaceContainer interfaces1 = address.Assign(devices1);
+	Ipv4AddressHelper address02;
+	address02.SetBase("10.1.2.0", "255.255.255.0");
+	Ipv4InterfaceContainer interfaces02 = address02.Assign(devices02);
 
-    address.SetBase("10.1.2.0", "255.255.255.0");
-    Ipv4InterfaceContainer interfaces2 = address.Assign(devices2);
+	UdpEchoServerHelper echoServer(9);
+	ApplicationContainer serverApps = echoServer.Install(nodes.Get(0));
+	serverApps.Start(Seconds(1.0));
+	serverApps.Stop(Seconds(10.0));
 
-    MobilityHelper mobility;
-    mobility.SetPositionAllocator("ns3::GridPositionAllocator",
-                                  "MinX", DoubleValue(350.0),
-                                  "MinY", DoubleValue(300.0),
-                                  "DeltaX", DoubleValue(150.0),
-                                  "DeltaY", DoubleValue(200.0),
-                                  "GridWidth", UintegerValue(3),
-                                  "LayoutType", StringValue("RowFirst"));
-    mobility.SetMobilityModel("ns3::ConstantPositionMobilityModel");
-    mobility.Install(nodes);
+	UdpEchoClientHelper echoClient01(interfaces01.GetAddress(0), 9);
+	echoClient01.SetAttribute("MaxPackets", UintegerValue(10));
+	echoClient01.SetAttribute("PacketSize", UintegerValue(1024));
+	echoClient01.SetAttribute("Interval", TimeValue(Seconds(1.0)));
 
-    UdpEchoServerHelper echoServer(9);
-    ApplicationContainer serverApps = echoServer.Install(nodes.Get(1));
-    serverApps.Start(Seconds(1.0));
-    serverApps.Stop(Seconds(10.0));
+	ApplicationContainer clientApps01 = echoClient01.Install(nodes.Get(1));
+	clientApps01.Start(Seconds(2.0));
+	clientApps01.Stop(Seconds(10.0));
 
-    UdpEchoClientHelper echoClient1(interfaces1.GetAddress(1), 9);
-    echoClient1.SetAttribute("MaxPackets", UintegerValue(3));
-    echoClient1.SetAttribute("Interval", TimeValue(Seconds(1.0)));
-    echoClient1.SetAttribute("PacketSize", UintegerValue(1024));
-    ApplicationContainer clientApps1 = echoClient1.Install(nodes.Get(0));
-    clientApps1.Start(Seconds(2.0));
-    clientApps1.Stop(Seconds(10.0));
+	UdpEchoClientHelper echoClient02(interfaces02.GetAddress(0), 9);
+	echoClient02.SetAttribute("MaxPackets", UintegerValue(10));
+	echoClient02.SetAttribute("PacketSize", UintegerValue(1024));
+	echoClient02.SetAttribute("Interval", TimeValue(Seconds(1.0)));
 
-    UdpEchoClientHelper echoClient2(interfaces2.GetAddress(1), 9);
-    echoClient2.SetAttribute("MaxPackets", UintegerValue(3));
-    echoClient2.SetAttribute("Interval", TimeValue(Seconds(1.0)));
-    echoClient2.SetAttribute("PacketSize", UintegerValue(1024));
-    ApplicationContainer clientApps2 = echoClient2.Install(nodes.Get(2));
-    clientApps2.Start(Seconds(3.0));
-    clientApps2.Stop(Seconds(10.0));
+	ApplicationContainer clientApps02 = echoClient02.Install(nodes.Get(2));
+	clientApps02.Start(Seconds(3.0));
+	clientApps02.Stop(Seconds(10.0));
 
-    AnimationInterface anim("Second.xml");
-    Simulator::Run();
-    Simulator::Destroy();
+	AsciiTraceHelper ascii;
+	pointToPoint.EnableAsciiAll(ascii.CreateFileStream("p2p-three-nodes-trace.tr"));
 
-    return 0;
+	AnimationInterface anim("ThreeNodesAnimation.xml");
+	anim.SetConstantPosition(nodes.Get(0), 50, 50);
+	anim.SetConstantPosition(nodes.Get(1), 0, 100);
+	anim.SetConstantPosition(nodes.Get(2), 100, 100);
+
+	anim.UpdateNodeDescription(nodes.Get(0), "Server");
+	anim.UpdateNodeDescription(nodes.Get(1), "Client");
+	anim.UpdateNodeDescription(nodes.Get(2), "Client");
+
+	anim.UpdateNodeColor(nodes.Get(0), 0, 255, 0);
+
+	Simulator::Run();
+	Simulator::Destroy();
+
+	return 0;
 }
